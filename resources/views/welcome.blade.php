f99b1af57aabd592f260f57022fcb001f6439c24
Edenilson Ramos <edenilson.ramos18@gmail.com>
2023-04-19 21:02:23 -0300

    feat: ajustes no css da pagina home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
         @foreach ($events as $event)
             <div class="card col-md-3">
                 <img src="img/event_placeholder.jpg" alt="{{ $event->title }}">
-                <div class="card body">
+                <div class="card-body">
                     <p class="card-date">10/09/2020</p>
                     <h5 class="card-title">{{ $event->title }}</h5>
                     <p class="card-participants">X Participantes</p>    
